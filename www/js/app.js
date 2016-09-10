cc4fd91725965ad9be072e4ed384f4bdd7c9b3d6
mmirotta <m.mirotta@gmail.com>
2016-09-10 09:24:46 -0300

    Se agrega icono y splash. Cambia el nombre de la app

--- www/js/app.js
+++ www/js/app.js
@@ -101,6 +101,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/juego');
+  $urlRouterProvider.otherwise('/tab/ingreso');
 
 });
